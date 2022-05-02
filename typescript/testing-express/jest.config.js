5df209c7f5c256c950d24dbd38795accdb707fd5
Manasa Ranjan Mishra <manas.mishra1@myntra.com>
2022-05-03 01:10:44 +0530

    Initial commit

--- typescript/testing-express/jest.config.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: path.join(__dirname, 'prisma', 'prisma-test-environment.js'),
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-}
